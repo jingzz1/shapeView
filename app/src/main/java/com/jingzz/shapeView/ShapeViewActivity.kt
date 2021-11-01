d568d4f8adc7ebb003610c2b1d74340b0cb108bc
jingzz <243436438@qq.com>
2021-11-01 11:27:52 +0800

    修改readme

--- app/src/main/java/com/jingzz/shapeView/ShapeViewActivity.kt
+++ app/src/main/java/com/jingzz/shapeView/ShapeViewActivity.kt
@@ -2,6 +2,7 @@ package com.jingzz.shapeView
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jingzz.shapeView.widget.ShapeView
 
 class ShapeViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
